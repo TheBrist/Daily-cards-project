8393c90031297fcb9c015249d11993025fffe628
Iloveronyeskin <ilove@ron.yeskin>
2025-04-22 17:48:03 +0300

    add and use env vars in backend

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,11 +1,14 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "daily_dashboard",
-  password: "postgres",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST, // Usually 'localhost' when using Cloud SQL Auth Proxy
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
+  ssl: {
+    rejectUnauthorized: false // Optional, depends on your Cloud SQL SSL settings
+  }
 });
 
 module.exports = pool;
