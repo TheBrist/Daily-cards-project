84935fb3f18f306b9bcfa94994d14d08653246a4
Iloveronyeskin <ilove@ron.yeskin>
2025-04-23 18:30:41 +0300

    add cloudrun to cors

--- server/index.js
+++ server/index.js
@@ -3,21 +3,22 @@ const express = require('express');
 const cors = require('cors');
 const PORT = process.env.PORT || 8080;
 
-const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:5173'];
+const whitelist = ['https://frontend-daily-cards-418901622719.me-west1.run.app', 'http://34.0.69.148'];
 const corsOptions = {
-    // origin: (origin, callback) => {
-    //     if (whitelist.indexOf(origin) !== -1 || !origin) {
-    //         callback(null, true);
-    //     } else {
-    //         callback(new Error('Not allowed by CORS'));
-    //     }
-    // },
-
-    origin: '*',
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     optionsSuccessStatus: 200
-}
+};
 
-const app = express(corsOptions);
+app.use(cors(corsOptions));
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
