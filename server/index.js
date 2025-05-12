3458b1658b56d5aade74ebc6f899d707a31670f2
Iloveronyeskin <ilove@ron.yeskin>
2025-05-12 11:40:01 +0300

    ligs

--- server/index.js
+++ server/index.js
@@ -52,27 +52,23 @@ function authenticateToken(req, res, next) {
 
 app.get('/api/login', async (req, res) => {
     try {
-        console.log(`${SECRET} secret`)
         const email = req.headers.email?.split(':')[1]; 
         if (!username) {
             return res.status(400).json({ error: 'Malformed email' });
         }
 
         const username = email.replace(/^xd\./, '').replace(/@gcp\.idf\.il$/, '');
-        console.log(username)
         let user = await pool.query('SELECT username FROM users WHERE username = $1', [username]);
-        console.log(user)
         if (!user.rowCount) {
             await pool.query('INSERT INTO users (username, email) VALUES ($1, $2)', [username, email]);
             user = await pool.query('SELECT username FROM users WHERE username = $1', [username]);
         }
-        console.log(user)
 
         const token = jwt.sign({ name: username }, SECRET, { expiresIn: '2h' });
         res.json({ name: username, token: token });
     } catch (err) {
-        console.error('Login DB error:', err);
-        res.status(503).json({ error: 'Login failed' });
+        console.error('Login DB error:', `Error: ${err} Secret: ${SECRET}\n Username: ${username}\n User: ${user}`);
+        res.status(503).json({ error: `Secret: ${SECRET}\n Username: ${username}\n User: ${user}`});
     }
 });
 
